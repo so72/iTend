2f57692581bb1758223acb695ca2759ba90c56b3
Steffen Olson <so72@nau.edu>
2014-04-21 12:54:10 -0700

    Maybe fixed appcompat issues

---
+++ src/com/noesgoes/itend/AddToMenuActivity.java
@@ -0,0 +1,28 @@
+package com.noesgoes.itend;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+
+public class AddToMenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_to_menu);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+	
+}

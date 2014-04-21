0b21f7197f5ade5e485394b7a8b6c0d8e210e434
Steffen Olson <so72@nau.edu>
2014-04-21 14:24:21 -0700

    finished a barebones activity for adding drinks to the menu

--- src/com/noesgoes/itend/AddToMenuActivity.java
+++ src/com/noesgoes/itend/AddToMenuActivity.java
@@ -4,15 +4,29 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddToMenuActivity extends Activity {
 
+	private Spinner typeSpinner;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_to_menu);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		typeSpinner = (Spinner) findViewById(R.id.drink_type);
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.drink_types_array, android.R.layout.simple_spinner_item);
+		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		typeSpinner.setAdapter(adapter);
 	}
 
 	/**
@@ -23,6 +37,13 @@ public class AddToMenuActivity extends Activity {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+	}
+	
+	public void onAddToMenuClicked(View view) {
+		// add drink to db
+		
+		
+		NavUtils.navigateUpFromSameTask(this);
 	}
 	
 }

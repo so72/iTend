39a5cb2c68aa8d8968ede6aad7c4d1888be9bc9a
Steffen Olson <sknoslo@gmail.com>
2014-04-30 19:24:13 -0700

    updated layout of all drink types

--- src/com/noesgoes/itend/BeerSelectionActivity.java
+++ src/com/noesgoes/itend/BeerSelectionActivity.java
@@ -35,10 +35,10 @@ public class BeerSelectionActivity extends ListActivity {
         startManagingCursor(beerCursor);
 
         // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] from = new String[]{DrinkDbAdapter.KEY_NAME, DrinkDbAdapter.KEY_DESCRIPTION};
+        String[] from = new String[]{DrinkDbAdapter.KEY_NAME, DrinkDbAdapter.KEY_DESCRIPTION, DrinkDbAdapter.KEY_COST};
 
         // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2};
+        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
 
         // Now create a simple cursor adapter and set it to display
         SimpleCursorAdapter beers = 
--- src/com/noesgoes/itend/MixedSelectionActivity.java
+++ src/com/noesgoes/itend/MixedSelectionActivity.java
@@ -34,10 +34,10 @@ public class MixedSelectionActivity extends ListActivity {
         startManagingCursor(notesCursor);
 
         // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] from = new String[]{DrinkDbAdapter.KEY_NAME};
+        String[] from = new String[]{DrinkDbAdapter.KEY_NAME, DrinkDbAdapter.KEY_DESCRIPTION, DrinkDbAdapter.KEY_COST};
 
         // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1};
+        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
 
         // Now create a simple cursor adapter and set it to display
         SimpleCursorAdapter beers = 
